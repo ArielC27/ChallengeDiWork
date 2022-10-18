03e948e3fd54ff4c24e4dcf955e71d4fd45c5e74
ArielC27 <109997773+ArielC27@users.noreply.github.com>
2022-10-18 11:16:53 -0300

    En proceso

---
+++ ChallengeDiWork/ChallengeDiWork/Modelos/BD.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeDiWork.Modelos
+{
+    public class BD
+    {
+        public const string ConnecctionString = "Server=DESKTOP-NU2KG89;Database=Taller;Trusted_Connection=True";
+    }
+}
