b4aa17d1e700c6af411fbe6bf161f80904548c73
ArielC27 <109997773+ArielC27@users.noreply.github.com>
2022-10-18 16:12:37 -0300

    En proceso

--- ChallengeDiWork/Modelo/Class1.cs
+++ ChallengeDiWork/Modelo/Class1.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ChallengeDiWork.Logica
+namespace Modelo
 {
-    public class Moto : Vehiculo
+    public class Class1
     {
-        public int Cilindrada { get; set; }
     }
 }
